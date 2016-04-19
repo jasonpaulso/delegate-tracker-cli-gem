d89fd32a525a49b52316de4b95cd9101077a1674
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-04-19 15:26:07 -0700

    further revisions while getting a handle on making and installing gems

--- lib/delegate_tracker.rb
+++ lib/delegate_tracker.rb
@@ -1,8 +1,5 @@
 require_relative "./delegate_tracker/version"
 
-# module DelegateTracker
-#   # Your code goes here...
-# end
 
 require_relative './delegate_tracker/cli'
 require_relative './delegate_tracker/candidate_list_scraper'
